4ca70b247c2ccb87f77b3981cb6b611f5c387033
Fabien Campagne <fac2003@campagnelab.org>
2016-09-01 16:47:24 -0400

    Latest version of simulate script.
    Former-commit-id: 95df83866213b135491fe1f68748734f835579a4

--- scripts/simulate/simulate-somatic-variations.sh
+++ scripts/simulate/simulate-somatic-variations.sh
@@ -10,16 +10,25 @@ reformat.sh in=Homo_sapiens.GRCh38.cdna.all.fa  out=sample.fasta minlength=200  
 mutate.sh in=sample.fasta out=mutated-snp-indels.fasta  subrate=0.004
 
 # Map to the genome, removing ambiguous mappers:
-bbmap.sh -Xmx13g in=mutated-snp-indels.fasta ref=/data/genomes/Homo_sapiens.ucsc.hg19.fa out=out.sam \
+bbmap.sh -Xmx13g build=5 in=mutated-snp-indels.fasta ref=/data/genomes/1000G.fa out=mutated-snp-indels.sam \
+                vslow=true  ambiguous=toss usemodulo=true maxindel=100000 xstag=firststrand intronlen=10
+
+# Map to the genome, removing ambiguous mappers:
+bbmap.sh -Xmx13g in=sample.fasta ref=/data/genomes/1000G.fa out=sample.sam \
                         ambiguous=toss usemodulo=true maxindel=100000 xstag=firststrand intronlen=10
 
 # Convert sam to goby format:
 export PATH=${PATH}:~/IdeaProjects/git/goby
-goby 4g stc -i mutated-snp-indels.sam -o mutated-snp-indels -g /data/genomes/Homo_sapiens.ucsc.hg19
+goby 4g stc -i mutated-snp-indels.sam -o mutated-snp-indels -g /data/genomes/1000G
+goby 4g stc -i sample.sam  -o sample -g /data/genomes/1000G
+
 
 goby 4g sort mutated-snp-indels.entries -o mutated-snp-indels-sorted
-goby 4g discover-sequence-variants mutated-snp-indels-sorted -f genotypes --genome /data/genomes/Homo_sapiens.ucsc.hg19 -o mutated-snp-indels.vcf --call-indels true -n 1 -t 1
-grep -v "0/0" mutated-snp-indels.vcf |grep -v '##' |cut -f 1,2,4,5 >true-mutations.tsv
+goby 4g sort sample.entries -o sample-sorted
+
+goby 4g discover-sequence-variants sample-sorted mutated-snp-indels-sorted -f genotypes --genome /data/genomes/1000G -o mutated-snp-indels.vcf --call-indels true -n 1 -t 1
+
+grep "0/0" mutated-snp-indels.vcf |grep "0/1" |grep -v '##' |cut -f 1,2,4,5 >true-mutations.tsv
 
 NUM_READS=10000000
 randomreads.sh build=2 ref=sample.fasta  out=germline-reads.fq reads=${NUM_READS} seed=9484 maxq=40 midq=35 minq=28
