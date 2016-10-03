73c21feb0510f3d072d99476597264ec2f5933d3
Manuele Simi <manuele.simi@campagnelab.org>
2016-10-03 11:23:13 -0400

    Prepare data zip archive for the release.

--- prepare-release.sh
+++ prepare-release.sh
@@ -4,9 +4,11 @@ VERSION="3.0.2"
 RELEASE_FOLDER="release-goby_${VERSION}"
 
 mkdir -p ${RELEASE_FOLDER}
-mvn -pl :goby-distribution clean package
+#mvn -pl :goby-distribution clean package
 cp goby-distribution/target/*-javadoc.jar ${RELEASE_FOLDER}/goby_${VERSION}-apidoc.zip
 cp goby-distribution/target/*-sources.jar ${RELEASE_FOLDER}/goby_${VERSION}-src.zip
 
 mvn -pl :goby-framework assembly:single@make-goby-models
+mvn -pl :goby-framework assembly:single@make-goby-data
+
 mv target/*.zip  ${RELEASE_FOLDER}
