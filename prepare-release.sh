b4985c1cc00b7d1620e7c57d4d21675e0ba51362
Manuele Simi <manuele.simi@campagnelab.org>
2016-10-03 11:14:20 -0400

    Add antrun config to goby-framework to execute the release script as maven goal.

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-mvn clean package
-mkdir -p release-files
-mv
+VERSION="3.0.2"
+RELEASE_FOLDER="release-goby_${VERSION}"
+
+mkdir -p ${RELEASE_FOLDER}
+mvn -pl :goby-distribution clean package
+cp goby-distribution/target/*-javadoc.jar ${RELEASE_FOLDER}/goby_${VERSION}-apidoc.zip
+cp goby-distribution/target/*-sources.jar ${RELEASE_FOLDER}/goby_${VERSION}-src.zip
+
 mvn -pl :goby-framework assembly:single@make-goby-models
-mv target/*.zip  release-files
+mv target/*.zip  ${RELEASE_FOLDER}
