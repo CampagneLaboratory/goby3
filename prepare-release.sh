7932032c3361c8aa0c86f1245fdb0d2237e09457
Manuele Simi <manuele.simi@campagnelab.org>
2016-10-03 12:42:30 -0400

    Create deps archive with the assembly plugin. Add to the release folder and create symlink in the folder.

--- prepare-release.sh
+++ prepare-release.sh
@@ -5,17 +5,19 @@ RELEASE_FOLDER="release-goby_${VERSION}"
 
 rm -rf ${RELEASE_FOLDER}
 mkdir -p ${RELEASE_FOLDER}
-mvn -pl :goby-distribution clean install
+#mvn -pl :goby-distribution clean install
 cp goby-distribution/target/*-javadoc.jar ${RELEASE_FOLDER}/goby_${VERSION}-apidoc.zip
 cp goby-distribution/target/*-sources.jar ${RELEASE_FOLDER}/goby_${VERSION}-src.zip
 cp goby.jar ${RELEASE_FOLDER}/goby.jar
 
 mvn -pl :goby-framework assembly:single@make-goby-models
 mvn -pl :goby-framework assembly:single@make-goby-data
+mvn -pl :goby-framework assembly:single@make-goby-deps
 
 mv target/*.zip  ${RELEASE_FOLDER}
 
 (cd ${RELEASE_FOLDER}; ln -s *-data.zip goby-data.zip)
+(cd ${RELEASE_FOLDER}; ln -s *-deps.zip goby-deps.zip)
 (cd ${RELEASE_FOLDER}; ln -s *-models.zip goby-models.zip)
 (cd ${RELEASE_FOLDER}; ln -s goby_${VERSION}-apidoc.zip goby-apidoc.zip)
 (cd ${RELEASE_FOLDER}; ln -s goby_${VERSION}-src.zip goby-src.zip)
