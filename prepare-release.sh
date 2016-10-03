ff40940b053d36e55964e0598eb2228c7b3aeb51
Manuele Simi <manuele.simi@campagnelab.org>
2016-10-03 12:34:44 -0400

    Add goby.jar to the release folder.

--- prepare-release.sh
+++ prepare-release.sh
@@ -3,10 +3,12 @@
 VERSION="3.0.2"
 RELEASE_FOLDER="release-goby_${VERSION}"
 
+rm -rf ${RELEASE_FOLDER}
 mkdir -p ${RELEASE_FOLDER}
-#mvn -pl :goby-distribution clean package
+mvn -pl :goby-distribution clean install
 cp goby-distribution/target/*-javadoc.jar ${RELEASE_FOLDER}/goby_${VERSION}-apidoc.zip
 cp goby-distribution/target/*-sources.jar ${RELEASE_FOLDER}/goby_${VERSION}-src.zip
+cp goby.jar ${RELEASE_FOLDER}/goby.jar
 
 mvn -pl :goby-framework assembly:single@make-goby-models
 mvn -pl :goby-framework assembly:single@make-goby-data
