9eb468b002afd9dc4733d3a45635fc792b61b028
Manuele Simi <manuele.simi@campagnelab.org>
2016-10-04 13:29:33 -0400

    Add commands to execute on the distribution website to the push script (as part of a remote ssh session).

--- formal-releases/_push-release.sh
+++ formal-releases/_push-release.sh
@@ -6,8 +6,16 @@ if [ -d "$RELEASE_FOLDER" ]; then
     # remove any previous archive for this release
     rm -f release-goby_${VERSION}.tgz
     tar zcvf release-goby_${VERSION}.tgz ${RELEASE_FOLDER}
-    #scp release-goby_${VERSION}.tgz www@okeeffe:/var/www/dirs/chagall/goby/releases/
-    #git tag "r${VERSION}"
+    scp release-goby_${VERSION}.tgz www@okeeffe:/var/www/dirs/chagall/goby/releases/
+    ssh www@okeeffe << END
+        cd /var/www/dirs/chagall/goby/releases/
+        tar zxvf release-goby_${VERSION}.tgz
+        rm -f latest-release
+        ln -s release-goby_${VERSION} latest-release
+        cd archive
+        ln -s ../release-goby_${VERSION} .
+END
+    git tag "r${VERSION}"
     #git push --tags
 else
     echo "$RELEASE_FOLDER does not exist. Please, run prepare-release.sh to prepare the release files for $VERSION and then try again."
