d4183aa2d006069459384b0bdb721774a5998012
Fabien Campagne <fac2003@campagnelab.org>
2016-11-08 12:34:57 -0500

    Use the hasNext method of the iterator, not of the reader.

--- goby-distribution/src/main/java/org/campagnelab/goby/baseinfo/SequenceBaseInformationReader.java
+++ goby-distribution/src/main/java/org/campagnelab/goby/baseinfo/SequenceBaseInformationReader.java
@@ -202,7 +202,7 @@ public class SequenceBaseInformationReader implements Iterator<BaseInformationRe
      * @return the next read entry from the input stream.
      */
     public final BaseInformationRecords.BaseInformation next() {
-        if (!reader.hasNext(collection, collection==null? 0: collection.getRecordsCount())) {
+        if (!hasNext()) {
             throw new NoSuchElementException();
         }
         final BaseInformationRecords.BaseInformation record = collection.getRecords(reader.incrementEntryIndex());
