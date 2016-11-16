59c57d41a0e2343ea14d36ceb78b3bf630ea38dd
Manuele Simi <manuele.simi@campagnelab.org>
2016-11-16 15:17:47 -0500

    Download and package somatic and framework artifacts with the snapshot release.

--- snapshot-previews/prepare-preview.sh
+++ snapshot-previews/prepare-preview.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash -x
 
 VERSION=$1
+DL_VERSION=$2
 if [[ ! $VERSION == *"SNAPSHOT"* ]] ;then
   echo "Current version is set to ${VERSION}, but you should only preview a snapshot (version must end in -SNAPSHOT)!";
   exit 1
 fi
 rm -fr package
+mvn dependency:copy -Ddl.version=${DL_VERSION} -U
 cd ..
 mvn clean package
 mkdir -p snapshot-previews/package
-cp goby.jar snapshot-previews/package/
+cp goby.jar somatic.jar framework.jar snapshot-previews/package/
 cp goby snapshot-previews/package/
 cp -r config snapshot-previews/package/
 cp -r models snapshot-previews/package/
