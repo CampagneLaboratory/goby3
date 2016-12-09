d1c803360f5956698ae5734d8d0349001942538d
Manuele Simi <manuele.simi@campagnelab.org>
2016-12-09 11:07:09 -0500

    Check if the dl version parameter is set in prepare-release.sh.

--- formal-releases/prepare-release.sh
+++ formal-releases/prepare-release.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 DL_VERSION=$1
+if [ -z "$DL_VERSION" ]; then
+    echo "ERROR: The '<dl.version>' parameter is missing a value."
+    echo "Syntax: ./prepare-release <dl.version>"
+    exit 1
+fi
 WORKDIR=`pwd`
 cd ..
-mvn -pl :goby-framework antrun:run@prepare-goby-release -Ddl.version=${DL_VERSION}
+mvn -pl :goby-framework antrun:run@prepare-goby-release -Ddl.version=$DL_VERSION
 cd ${WORKDIR}
+
+
+
+
