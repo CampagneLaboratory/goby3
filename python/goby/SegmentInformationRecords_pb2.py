ab5a766bd24f762fd4cb9223a552e08f1493c238
Manuele Simi <manuele.simi@campagnelab.org>
2017-11-22 11:39:11 -0500

    Store only the location for each base in the segment. Chromosome and ref index can be extracted from the start/end positions of the segment.

--- python/goby/SegmentInformationRecords_pb2.py
+++ python/goby/SegmentInformationRecords_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='SegmentInformationRecords.proto',
   package='org.campagnelab.dl.varanalysis.protobuf',
   syntax='proto2',
-  serialized_pb=_b('\n\x1fSegmentInformationRecords.proto\x12\'org.campagnelab.dl.varanalysis.protobuf\"l\n\x1cSegmentInformationCollection\x12L\n\x07records\x18\x01 \x03(\x0b\x32;.org.campagnelab.dl.varanalysis.protobuf.SegmentInformation\"\x8b\x02\n\x12SegmentInformation\x12R\n\x0estart_position\x18\x01 \x01(\x0b\x32:.org.campagnelab.dl.varanalysis.protobuf.ReferencePosition\x12P\n\x0c\x65nd_position\x18\x02 \x01(\x0b\x32:.org.campagnelab.dl.varanalysis.protobuf.ReferencePosition\x12\x0e\n\x06length\x18\x05 \x01(\r\x12?\n\x06sample\x18\x04 \x03(\x0b\x32/.org.campagnelab.dl.varanalysis.protobuf.Sample\"E\n\x06Sample\x12;\n\x04\x62\x61se\x18\x01 \x03(\x0b\x32-.org.campagnelab.dl.varanalysis.protobuf.Base\"\xc1\x02\n\x04\x42\x61se\x12\x10\n\x08\x66\x65\x61tures\x18\x01 \x03(\x02\x12\x0e\n\x06labels\x18\x02 \x03(\x02\x12\r\n\x05\x63olor\x18\x06 \x03(\r\x12\x11\n\ttrueLabel\x18\x03 \x03(\t\x12!\n\x19prePostProcessingGenotype\x18\n \x01(\t\x12\x0e\n\x06offset\x18\x0b \x01(\r\x12\x19\n\x11hasCandidateIndel\x18\x04 \x01(\x08\x12\x14\n\x0chasTrueIndel\x18\x05 \x01(\x08\x12\x11\n\tisVariant\x18\x07 \x01(\x08\x12\x17\n\x0freferenceAllele\x18\x08 \x01(\t\x12\x17\n\x0f\x66ormattedCounts\x18\t \x01(\t\x12L\n\x08position\x18\x0c \x01(\x0b\x32:.org.campagnelab.dl.varanalysis.protobuf.ReferencePosition\"T\n\x11ReferencePosition\x12\x17\n\x0freference_index\x18\x02 \x01(\r\x12\x14\n\x0creference_id\x18\x03 \x01(\t\x12\x10\n\x08location\x18\x01 \x01(\rB+\n\'org.campagnelab.dl.varanalysis.protobufH\x01')
+  serialized_pb=_b('\n\x1fSegmentInformationRecords.proto\x12\'org.campagnelab.dl.varanalysis.protobuf\"l\n\x1cSegmentInformationCollection\x12L\n\x07records\x18\x01 \x03(\x0b\x32;.org.campagnelab.dl.varanalysis.protobuf.SegmentInformation\"\x8b\x02\n\x12SegmentInformation\x12R\n\x0estart_position\x18\x01 \x01(\x0b\x32:.org.campagnelab.dl.varanalysis.protobuf.ReferencePosition\x12P\n\x0c\x65nd_position\x18\x02 \x01(\x0b\x32:.org.campagnelab.dl.varanalysis.protobuf.ReferencePosition\x12\x0e\n\x06length\x18\x05 \x01(\r\x12?\n\x06sample\x18\x04 \x03(\x0b\x32/.org.campagnelab.dl.varanalysis.protobuf.Sample\"E\n\x06Sample\x12;\n\x04\x62\x61se\x18\x01 \x03(\x0b\x32-.org.campagnelab.dl.varanalysis.protobuf.Base\"\x85\x02\n\x04\x42\x61se\x12\x10\n\x08\x66\x65\x61tures\x18\x01 \x03(\x02\x12\x0e\n\x06labels\x18\x02 \x03(\x02\x12\r\n\x05\x63olor\x18\x06 \x03(\r\x12\x11\n\ttrueLabel\x18\x03 \x03(\t\x12!\n\x19prePostProcessingGenotype\x18\n \x01(\t\x12\x0e\n\x06offset\x18\x0b \x01(\r\x12\x19\n\x11hasCandidateIndel\x18\x04 \x01(\x08\x12\x14\n\x0chasTrueIndel\x18\x05 \x01(\x08\x12\x11\n\tisVariant\x18\x07 \x01(\x08\x12\x17\n\x0freferenceAllele\x18\x08 \x01(\t\x12\x17\n\x0f\x66ormattedCounts\x18\t \x01(\t\x12\x10\n\x08location\x18\x0c \x01(\r\"T\n\x11ReferencePosition\x12\x17\n\x0freference_index\x18\x02 \x01(\r\x12\x14\n\x0creference_id\x18\x03 \x01(\t\x12\x10\n\x08location\x18\x01 \x01(\rB+\n\'org.campagnelab.dl.varanalysis.protobufH\x01')
 )
 
 
@@ -224,9 +224,9 @@ _BASE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None),
     _descriptor.FieldDescriptor(
-      name='position', full_name='org.campagnelab.dl.varanalysis.protobuf.Base.position', index=11,
-      number=12, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
+      name='location', full_name='org.campagnelab.dl.varanalysis.protobuf.Base.location', index=11,
+      number=12, type=13, cpp_type=3, label=1,
+      has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
@@ -243,7 +243,7 @@ _BASE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=528,
-  serialized_end=849,
+  serialized_end=789,
 )
 
 
@@ -287,8 +287,8 @@ _REFERENCEPOSITION = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=851,
-  serialized_end=935,
+  serialized_start=791,
+  serialized_end=875,
 )
 
 _SEGMENTINFORMATIONCOLLECTION.fields_by_name['records'].message_type = _SEGMENTINFORMATION
@@ -296,7 +296,6 @@ _SEGMENTINFORMATION.fields_by_name['start_position'].message_type = _REFERENCEPO
 _SEGMENTINFORMATION.fields_by_name['end_position'].message_type = _REFERENCEPOSITION
 _SEGMENTINFORMATION.fields_by_name['sample'].message_type = _SAMPLE
 _SAMPLE.fields_by_name['base'].message_type = _BASE
-_BASE.fields_by_name['position'].message_type = _REFERENCEPOSITION
 DESCRIPTOR.message_types_by_name['SegmentInformationCollection'] = _SEGMENTINFORMATIONCOLLECTION
 DESCRIPTOR.message_types_by_name['SegmentInformation'] = _SEGMENTINFORMATION
 DESCRIPTOR.message_types_by_name['Sample'] = _SAMPLE
